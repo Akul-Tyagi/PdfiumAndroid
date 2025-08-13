71e9f7e0fa7c642402f3391ae4e6afd855dce459
Akul Tyagi <akultyagi7@gmail.com>
2025-08-13 17:00:20 +0530

    16KB compliant builds

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -1,13 +1,7 @@
 APP_STL := c++_shared
 APP_CPPFLAGS += -fexceptions
+APP_PLATFORM := android-21
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 
-# Add page size flags to support 16KB pages
+# 16KB compatibility flag
 APP_LDFLAGS += -Wl,-z,max-page-size=16384
-
-#For ANativeWindow support
-APP_PLATFORM = android-19
-
-APP_ABI :=  armeabi-v7a \
-            arm64-v8a \
-            x86 \
-            x86_64
